fb0ba3261a82fcd772a1b725c520f83885bf0033
Arthur <arthurd800@wp.pl>
2020-03-02 11:20:46 +0100

    some minor changes and fixes

--- functions/index.js
+++ functions/index.js
@@ -1,6 +1,6 @@
 const functions = require('firebase-functions');
 const app= require('express')();
-const {getAllScreams,postOneScream,getScream,commentOnScream} = require('./handlers/screams');
+const {getAllScreams,postOneScream,getScream,commentOnScream,likeScream,unlikeScream,deleteScream} = require('./handlers/screams');
 const {signup,login,uploadImage,addUserDetails,getAuthenticatedUser} = require('./handlers/users');
 const {FBAuth} = require('./util/fbAuth');
 
@@ -9,6 +9,9 @@ app.get('/screams',getAllScreams);
 app.post('/scream',FBAuth,postOneScream);
 app.get('/scream/:screamId',getScream);
 app.post('/scream/:screamId/comment',FBAuth,commentOnScream);
+app.get('/scream/:screamId/like',FBAuth,likeScream);
+app.get('/scream/:screamId/unlike',FBAuth,unlikeScream);
+app.delete('/scream/:screamId',FBAuth,deleteScream);
 // User routes
 app.post('/signup',signup);
 app.post('/login',login);
